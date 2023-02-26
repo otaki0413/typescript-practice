d946130604f74f2e16f071cc4cc09a0cdf0da92c
otaki0413 <takida0413@gmail.com>
2023-02-27 00:50:00 +0900

    ✨ プリミティブ型の型変換について学習

---
+++ src/2.3.9_プリミティブ型同士の暗黙的変換/index.ts
@@ -0,0 +1,12 @@
+import { createInterface } from "readline";
+
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("文字列を入力してください:", (line) => {
+  const result = line + 1000;
+  console.log(result);
+  rl.close();
+});
